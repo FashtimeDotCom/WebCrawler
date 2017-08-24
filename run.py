8b2135f511b3d0b31491b413fee5b349a0fc4858
ayou <454968095@qq.com>
2017-08-24 16:16:43 +0800

    update run.py
    修复loop关闭问题

--- run.py
+++ run.py
@@ -5,15 +5,30 @@
 import threading
 import time
 from spiders.java_51cto import Cto_Spider
+from spiders.java_codeceo import Codeceo_Spider
+from spiders.java_codeceo_wd import Codeceo_Wd_Spider
+from spiders.java_csdn import Csdn_Spider
 
 start = time.time()
 
 # 将爬虫项目添加进列表中
-spider_list = [Cto_Spider().main(),]
+spider_list = [
+    Cto_Spider(),
+    Codeceo_Spider(),
+    Codeceo_Wd_Spider(),
+    Csdn_Spider(),
 
-# 有几个爬虫就开几个线程
-for i in spider_list:
-    t =threading.Thread(target=i)
+
+]
+
+threads = []
+
+for spider in spider_list:
+    t = threading.Thread(target=spider.main())
+    threads.append(t)
+
+for t in threads:
+    t.setDaemon(True)
     t.start()
 
 print("%s Elapsed Time: %s" % ("run.main",time.time() - start))
